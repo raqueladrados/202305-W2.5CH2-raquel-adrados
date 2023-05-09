7396c72ef242b29b996ac1dbab7726506117e504
Raquel Adrados <radrados00@gmail.com>
2023-05-09 19:07:37 +0200

    Add class of character and its test

---
+++ GoT_classes/character.js
@@ -0,0 +1,16 @@
+export class Character {
+  name;
+  family;
+  age;
+  status;
+  die;
+  series;
+  constructor(name, family, age, status, die, series) {
+    this.name = name;
+    this.family = family;
+    this.age = age;
+    this.status = status;
+    this.die = die;
+    this.series = series;
+  }
+}
---
+++ GoT_classes/character.test.js
@@ -0,0 +1,16 @@
+import { Character } from "./character";
+
+describe("Given Character class", () => {
+  describe("When we instanciate it", () => {
+    test("Then should have an object with properties of the class", () => {
+      const character = new Character();
+
+      expect(character).toHaveProperty("name");
+      expect(character).toHaveProperty("family");
+      expect(character).toHaveProperty("age");
+      expect(character).toHaveProperty("status");
+      expect(character).toHaveProperty("die");
+      expect(character).toHaveProperty("series");
+    });
+  });
+});
